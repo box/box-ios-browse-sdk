d7373b2b534ef62d730fb52a71ca3b4c933c0a4a
Andrew Dempsey <adempsey@box.com>
2015-11-10 12:51:02 -0800

    IOS-10020 - load images directly from resources

--- BoxBrowseSDK/BoxBrowseSDK/UIImage+BOXBrowseSDKAdditions.m
+++ BoxBrowseSDK/BoxBrowseSDK/UIImage+BOXBrowseSDKAdditions.m
@@ -10,29 +10,6 @@
 
 @implementation UIImage (BOXBrowseSDKAdditions)
 
-+ (UIImage *)box_imageFromBrowseSDKResourceBundleNamed:(NSString *)name
-{
-    UIImage *imageFromBundle = nil;
-    NSBundle *bundle = [self resourcesBundle];
-
-    if ([self respondsToSelector:@selector(imageNamed:inBundle:compatibleWithTraitCollection:)]) {
-        imageFromBundle = [self imageNamed:name inBundle:bundle compatibleWithTraitCollection:nil];
-    } else {
-        NSString *path = [[bundle resourcePath] stringByAppendingPathComponent:[bundle pathForResource:name ofType:nil]];
-
-        NSString *fileName = nil;
-        if ([UIScreen mainScreen].scale == 2.0) {
-            fileName = [NSString stringWithFormat:@"%@@2x.png", name];
-        } else {
-            fileName = [NSString stringWithFormat:@"%@.png", name];
-        }
-        path = [path stringByAppendingPathComponent:fileName];
-        imageFromBundle = [UIImage imageWithContentsOfFile:path];
-    }
-
-    return imageFromBundle;
-}
-
 - (UIImage *)box_imageAtAppropriateScaleFactor
 {
     UIImage *image = self;
@@ -42,18 +19,6 @@
     }
 
     return image;
-}
-
-+ (NSBundle *)resourcesBundle
-{
-    static NSBundle *frameworkBundle = nil;
-    static dispatch_once_t predicate;
-    dispatch_once(&predicate, ^{
-        NSURL *resourcesBundleURL = [[NSBundle mainBundle] URLForResource:@"BoxBrowseSDKResources" withExtension:@"bundle"];
-        frameworkBundle = [NSBundle bundleWithURL:resourcesBundleURL];
-    });
-    
-    return frameworkBundle;
 }
 
 @end
