b936db7932e6c4b7c03d6d37719e884c6d458833
Scott Liu <sliu@box.com>
2017-03-08 14:49:39 -0800

    Expose more headerfiles in BoxBrowseSDK.h

--- BoxBrowseSDK/BoxBrowseSDK/BoxBrowseSDK.h
+++ BoxBrowseSDK/BoxBrowseSDK/BoxBrowseSDK.h
@@ -1,16 +1,20 @@
-//
-//  BoxBrowseSDK.h
-//  BoxBrowseSDK
-//
-//  Created by Rico Yao on 3/24/15.
-//  Copyright (c) 2015 BOX. All rights reserved.
-//
+#import <UIKit/UIKit.h>
 
-#import <Foundation/Foundation.h>
+#import "BoxBrowseSDK.h"
+#import "BOXBrowseSDKConstants.h"
+#import "BOXThumbnailCache.h"
+#import "DummyStrings.h"
+#import "BOXItem+BOXBrowseSDKAdditions.h"
+#import "NSBundle+BOXBrowseSDKAdditions.h"
+#import "UIImage+BOXBrowseSDKAdditions.h"
+#import "BOXCreateFolderViewController.h"
 #import "BOXFolderViewController.h"
+#import "BOXItemsViewController.h"
+#import "BOXSearchResultsViewController.h"
+#import "BOXBrowseSDKFileTypeHelper.h"
+#import "BOXBrowseSDKResourceLocator.h"
+#import "BOXItemCell.h"
 
-@import BoxContentSDK;
+FOUNDATION_EXPORT double BoxBrowseSDKVersionNumber;
+FOUNDATION_EXPORT const unsigned char BoxBrowseSDKVersionString[];
 
-@interface BoxBrowseSDK : NSObject
-
-@end
